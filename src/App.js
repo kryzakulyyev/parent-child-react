462cc644c104757ba9c27e2cda5a486067308db8
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2021-01-02 12:43:34 -0800

    Created Hello Guest

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App()  {
+  return (
+   <div>
+     <h1> Hello Guest</h1>
+     <label htmlFor="user_name'">Enter your name:</label> <br/>
+     <input id='user_name' type='text'/> 
+   </div>
+   
+    );
+  }
+
+
+
+export default App;
