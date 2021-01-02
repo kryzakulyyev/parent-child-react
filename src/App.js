7ea15d7fc7ae8f4236cd4ad04a9236beb5c10872
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2021-01-02 13:45:26 -0800

    Print guest when it is empty

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
 class App extends React.Component  {
+  constructor(props){
+    super(props);
+    this.state = {
+      userName : 'Guest'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+
+  handleChange(e){
+   this.setState({
+     userName: e.target.value.trim() ? e.target.value: 'Guest'
+   })
+  }
+
   render(){
   return (
    <div>
-     <h1> Hello Guest</h1>
+     <h1> Hello {this.state.userName}</h1>
      <label htmlFor="user_name'">Enter your name:</label> <br/>
-     <input id='user_name' type='text'/> 
+     <input id='user_name' type='text' onKeyDown={this.handleChange}/> 
    </div>
    
     );
